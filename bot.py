025e0b2554408e13b7ba5dbaf6ca1a7cea80f5ba
Billy Gigurtsis <billy.gigurtsis@heythropcollege.ac.uk>
2021-02-02 21:52:42 +0000

    a

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 # bot.py
 import os
+import re
+
 import discord
-import io 
-import aiohttp
 
 from dotenv import load_dotenv
 
@@ -11,28 +11,26 @@ load_dotenv()
 client = discord.Client()
 
 @client.event
+async def on_message(messagetwo):
+    if message.content.startswith('hello'):
+        await message.channel.send('Hello!')
 
-#function to check for regex matching a url
-def match_url(url):
-       regex = re.compile(
-           "(([\w]+:)?//)?(([\d\w]|%[a-fA-f\d]{2,2})+(:([\d\w]|%[a-fA-f\d]{2,2})+)?@)?([\d\w][-\d\w]{0,253}[\d\w]\.)+[\w]{2,63}(:[\d]+)?(/([-+_~.\d\w]|%[a-fA-f\d]{2,2})*)*(\?(&?([-+_~.\d\w]|%[a-fA-f\d]{2,2})=?)*)?(#([-+_~.\d\w]|%[a-fA-f\d]{2,2})*)?"
-       )
-       if regex.match(url):
-           return True
-       else:
-           return False
-
+@client.event
+# function to check for regex matching a url
 async def on_message(message):
-
-	#check for max's user id and if the message match's the above regex
-	if message.id == 389104618099048468 and match_url(message) = true:
-
-		#download the image and send it, it's much easier if you upload the image from your pc vs from a url
-		async with aiohttp.ClientSession() as session:
-    		async with session.get(https://i.imgur.com/XHBa71T.jpg) as resp:
-        		if resp.status != 200:
-            		return await channel.send('Could not download file...')
-        		data = io.BytesIO(await resp.read())
-        		await channel.send(file=discord.File(data, 'bitch.png'))
+    def match_url(url):
+        regex = re.compile(
+            "(([\w]+:)?//)?(([\d\w]|%[a-fA-f\d]{2,2})+(:([\d\w]|%[a-fA-f\d]{2,2})+)?@)?([\d\w][-\d\w]{0,253}[\d\w]\.)+[\w]{2,63}(:[\d]+)?(/([-+_~.\d\w]|%[a-fA-f\d]{2,2})*)*(\?(&?([-+_~.\d\w]|%[a-fA-f\d]{2,2})=?)*)?(#([-+_~.\d\w]|%[a-fA-f\d]{2,2})*)?","",str()
+        )
+        if regex.match(url):
+            return True
+        else:
+            return False
+    # check for max's user id and if the message match's the above regex
+    if message.author.id == 143161331866927104:
+        print('test condition 1')
+        if match_url(message):
+            print('test condition 2')
+            await discord.channel.send(file=discord.File('bitch.jpg'))
 
 client.run(os.getenv('TOKEN'))
