9cbaa63b7788380ae2e4553bb04fee27b0968327
Billy Gigurtsis <billy.gigurtsis@heythropcollege.ac.uk>
2021-02-03 00:08:04 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,19 +18,13 @@ async def on_message(messagetwo):
 @client.event
 # function to check for regex matching a url
 async def on_message(message):
-    def match_url(url):
-        regex = re.compile(
-            "(([\w]+:)?//)?(([\d\w]|%[a-fA-f\d]{2,2})+(:([\d\w]|%[a-fA-f\d]{2,2})+)?@)?([\d\w][-\d\w]{0,253}[\d\w]\.)+[\w]{2,63}(:[\d]+)?(/([-+_~.\d\w]|%[a-fA-f\d]{2,2})*)*(\?(&?([-+_~.\d\w]|%[a-fA-f\d]{2,2})=?)*)?(#([-+_~.\d\w]|%[a-fA-f\d]{2,2})*)?","",str()
-        )
-        if regex.match(url):
-            return True
-        else:
-            return False
     # check for max's user id and if the message match's the above regex
-    if message.author.id == 143161331866927104:
-        print('test condition 1')
-        if match_url(message):
-            print('test condition 2')
-            await discord.channel.send(file=discord.File('bitch.jpg'))
+    regex = re.compile('((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*')
+    match = regex.search(message.content)#
+
+    if message.author.bot:
+        return
+    elif message.author.id == 389104618099048468 and match or message.attachments:
+        await message.channel.send(file=discord.File('bitch.jpg'))
 
 client.run(os.getenv('TOKEN'))
