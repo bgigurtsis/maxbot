ec879ffc4f1139f059e9066f46fc010f0dfce300
Billy Gigurtsis <billy.gigurtsis@heythropcollege.ac.uk>
2021-02-03 18:48:51 +0000

    env

--- bot.py
+++ bot.py
@@ -11,20 +11,18 @@ load_dotenv()
 client = discord.Client()
 
 @client.event
-async def on_message(messagetwo):
-    if message.content.startswith('hello'):
-        await message.channel.send('Hello!')
-
-@client.event
 # function to check for regex matching a url
 async def on_message(message):
-    # check for max's user id and if the message match's the above regex
+
+    emoji = '<:timeout2:806320653451001856>'
     regex = re.compile('((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*')
     match = regex.search(message.content)#
 
     if message.author.bot:
         return
-    elif message.author.id == 389104618099048468 and match or message.attachments:
-        await message.channel.send(file=discord.File('bitch.jpg'))
+    elif message.author.id == 389104618099048468 and match or message.attachments: 
+        await message.add_reaction(emoji)
 
 client.run(os.getenv('TOKEN'))
+
+
